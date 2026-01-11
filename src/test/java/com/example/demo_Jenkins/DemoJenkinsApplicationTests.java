d41b6ce365e10d616dc2c51060f50dd21fbfb0fd
AditMagilHub <adit.khurana@magilhub.com>
2026-01-11 13:04:01 +0530

    first commit

---
+++ src/test/java/com/example/demo_Jenkins/DemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_Jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
